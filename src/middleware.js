04055b170f6cfc8d6008470b4842ec94ee818dcd
David Yusta <david.yusta@guidesmiths.com>
2023-05-14 12:53:13 +0200

    refactor: replace args middleware by a trim for failOn and generateOutputOn

--- src/middleware.js
+++ src/middleware.js
@@ -1,20 +1,7 @@
-const { parseFailOnArgs } = require('./utils');
+const trimLicenseArgs = args => ({
+  ...args,
+  failOn: args.failOn?.trim(),
+  generateOutputOn: args.generateOutputOn?.trim()
+});
 
-const parseRegexLike = args => {
-  const { valid: validArgs, invalid } = parseFailOnArgs(args.failOn || []);
-  if (invalid.length) throw new Error(`The following args passed to --failOn are not valid: ${invalid.join(' ')}`);
-  return { ...args, failOn: validArgs };
-};
-
-const parseGenerateOutputOn = args => {
-  const { generateOutputOn } = args;
-  const parsedOutputOn = generateOutputOn ? generateOutputOn.split(',').map(p => p.trim()) : [];
-  return { ...args, generateOutputOn: parsedOutputOn };
-};
-
-const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
-
-module.exports = args => pipe(
-  parseRegexLike,
-  parseGenerateOutputOn
-)(args);
+module.exports = trimLicenseArgs;
