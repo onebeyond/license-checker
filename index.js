50871320f0a2a400c5e1e76452631144a12df15c
David Yusta <david.yusta@guidesmiths.com>
2023-05-14 12:50:40 +0200

    refactor: move checkLicense logic out of the index

--- index.js
+++ index.js
@@ -2,22 +2,13 @@
 
 const checker = require('./src/checker');
 const args = require('./src/args');
+const reporter = require('./src/reporter');
 const middleware = require('./src/middleware');
 const runner = require('./src/runner');
-const spdxValidator = require('./src/spdx-validator');
 
 (async () => {
   try {
-    const parsedArgs = middleware(args);
-    if (args.checkLicense) {
-      const license = args.checkLicense.trim();
-      spdxValidator.isSpdxCompliant(license);
-      console.info(`License ${license} is SPDX compliant`);
-      return;
-    }
-
-    await runner.run(checker, parsedArgs);
-    console.info('License check completed! No forbidden licenses packages found.');
+    await runner.run(checker, reporter, middleware(args));
   } catch (e) {
     console.error(e.message);
     process.exit(1);
