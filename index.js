824b32ce0dd40ba0939eb31e318744cb12ec46e2
David Yusta <david.yusta@guidesmiths.com>
2023-05-16 11:28:48 +0200

    refactor: remove args middleware

--- index.js
+++ index.js
@@ -3,12 +3,11 @@
 const checker = require('./src/checker');
 const args = require('./src/args');
 const reporter = require('./src/reporter');
-const middleware = require('./src/middleware');
 const runner = require('./src/runner');
 
 (async () => {
   try {
-    await runner.run(checker, reporter, middleware(args));
+    await runner.run(checker, reporter, args);
   } catch (e) {
     console.error(e.message);
     process.exit(1);
--- src/middleware.js
+++
@@ -1,7 +0,0 @@
-const trimLicenseArgs = args => ({
-  ...args,
-  failOn: args.failOn?.trim(),
-  generateOutputOn: args.generateOutputOn?.trim()
-});
-
-module.exports = trimLicenseArgs;
