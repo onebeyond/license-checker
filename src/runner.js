b20e0ec0638a8cd078d72b746917ec22e0fd7ded
David Yusta <david.yusta@guidesmiths.com>
2023-05-19 10:04:08 +0200

    refactor: extract functions to utils

--- src/runner.js
+++ src/runner.js
@@ -1,25 +1,8 @@
 const isSPDXCompliant = require('spdx-expression-validate');
-const satisfiesSPDXLicense = require('spdx-satisfies');
 
 const {
-  getPackageInfoList, formatForbiddenLicenseError
+  getPackageInfoList, formatForbiddenLicenseError, generateSPDXExpression, checkSPDXCompliance, checkPackagesLicenses
 } = require('./utils');
-
-const checkLicenseArgument = (args = []) => {
-  const invalidLicenses = args.filter(arg => !isSPDXCompliant(arg));
-  if (invalidLicenses.length) {
-    throw new Error(`The following licenses are not SPDX compliant. Please, use the --checkLicense option to validate your input:\n${invalidLicenses.join(' | ')}`);
-  }
-};
-
-const generateSPDXExpression = (args = []) => args.join(' OR ');
-
-const checkPackagesLicenses = (inputLicenses, packages) => packages.reduce((total, pkg) => {
-  const { licenses } = pkg;
-  if (!isSPDXCompliant(licenses)) return { ...total, warning: [...total.warning, pkg] };
-  if (inputLicenses && satisfiesSPDXLicense(inputLicenses, licenses)) return { ...total, error: [...total.error, pkg] };
-  return total;
-}, { error: [], warning: [] });
 
 const run = async (checker, reporter, args) => {
   if (args.checkLicense) {
@@ -32,8 +15,8 @@ const run = async (checker, reporter, args) => {
   }
 
   const { failOn, generateOutputOn } = args;
-  checkLicenseArgument(failOn);
-  checkLicenseArgument(generateOutputOn);
+  checkSPDXCompliance(failOn);
+  checkSPDXCompliance(generateOutputOn);
   const bannedLicenses = generateSPDXExpression(failOn);
   const generateOutputOnLicenses = generateSPDXExpression(generateOutputOn);
 
@@ -41,7 +24,7 @@ const run = async (checker, reporter, args) => {
 
   const packageList = getPackageInfoList(packages);
 
-  const { error: forbiddenPackages, warning: invalidPackages } = checkPackagesLicenses(bannedLicenses, packageList);
+  const { satisfied: forbiddenPackages, nonCompliant: invalidPackages } = checkPackagesLicenses(bannedLicenses, packageList);
   if (invalidPackages.length) {
     console.info(`The following package licenses are not SPDX compliant and cannot be validated:\n${invalidPackages.map(pkg => ` > ${pkg.package} | ${pkg.licenses}`).join('\n')}`);
   }
@@ -54,7 +37,7 @@ const run = async (checker, reporter, args) => {
   console.info('License check completed! No forbidden licenses packages found.');
 
   if (args.disableReport) return;
-  const { error: packagesIncludeOutputLicenses } = checkPackagesLicenses(generateOutputOnLicenses, packageList);
+  const { satisfied: packagesIncludeOutputLicenses } = checkPackagesLicenses(generateOutputOnLicenses, packageList);
   if (generateOutputOnLicenses && !packagesIncludeOutputLicenses.length) return;
   reporter.writeReportFile(args.outputFileName, packageList, args.customHeader);
 };
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,6 @@
+const isSPDXCompliant = require('spdx-expression-validate');
+const satisfiesSPDXLicense = require('spdx-satisfies');
+
 /**
  * Generate objects with information on each package that we want to include
  * in the report files. This will include metadata such as the package name,
@@ -43,7 +46,47 @@ const formatForbiddenLicenseError = licenses => {
   return `${header}\n${lines}`;
 };
 
+/**
+ * Generates a SPDX expression joining the input licenses with the OR operator
+ * @param {string[]} licenses - List of SPDX licenses
+ * @return {string} - A string with the input licenses joined with the OR operator.
+ * e.g. input -> ['MIT', 'GPL-1.0'] output -> 'MIT OR GPL-1.0'
+ */
+const generateSPDXExpression = (licenses = []) => licenses.join(' OR ');
+
+/**
+ * Checks whether the input licenses are SPDX compliant. Throws an error with the invalid licenses
+ * @param licenses - List of licenses
+ */
+const checkSPDXCompliance = (licenses = []) => {
+  const invalidLicenses = licenses.filter(arg => !isSPDXCompliant(arg));
+  if (invalidLicenses.length) {
+    throw new Error(
+      `The following licenses are not SPDX compliant. Please, use the --checkLicense option to validate your input:\n${invalidLicenses.join(' | ')}`
+    );
+  }
+};
+
+/**
+ * Checks the SPDX expression against the list of packages. If the license of the package itself is not SPDX compliant, the package will
+ * be included on the "nonCompliant" list. If the SPDX expression satisfies the package license, the package will be included on the
+ * "satisfied" list
+ * @param {string} expression - A SPDX expression
+ * @param {object[]} packages - A list of packages to be checked against the SPDX expression
+ * @return {{satisfied: object[], nonCompliant: object[]}} - A couple of lists including the packages that satisfy the SPDX expression
+ * and the packages with a non SPDX compliant license
+ */
+const checkPackagesLicenses = (expression, packages) => packages.reduce((total, pkg) => {
+  const { licenses } = pkg;
+  if (!isSPDXCompliant(licenses)) return { ...total, nonCompliant: [...total.nonCompliant, pkg] };
+  if (expression && satisfiesSPDXLicense(expression, licenses)) return { ...total, satisfied: [...total.satisfied, pkg] };
+  return total;
+}, { satisfied: [], nonCompliant: [] });
+
 module.exports = {
   getPackageInfoList,
-  formatForbiddenLicenseError
+  formatForbiddenLicenseError,
+  generateSPDXExpression,
+  checkSPDXCompliance,
+  checkPackagesLicenses
 };
