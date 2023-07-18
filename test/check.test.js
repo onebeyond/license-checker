4adec0f40660a0443d1fcaaa77fde70784f53d8c
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2023-07-18 18:32:42 +0200

    test: fix tests for check command

---
+++ test/check.test.js
@@ -0,0 +1,46 @@
+const { check } = require('../src/runner');
+
+describe('check command', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should throw an error if the license is not SPDX compliant', async () => {
+    const license = 'GPL';
+
+    let error;
+    try {
+      await check(license);
+    } catch (e) {
+      error = e;
+    } finally {
+      expect(error.message).toMatch(`Error: License "${license}" is not SPDX compliant`);
+    }
+  });
+
+  it('should not throw an error if the license is SPDX compliant', async () => {
+    const license = 'MIT';
+
+    let error;
+    try {
+      await check(license);
+    } catch (e) {
+      error = e;
+    } finally {
+      expect(error).toBeUndefined();
+    }
+  });
+
+  it('should throw an error if the alicense is one of the licenses not supported by the spdx-satisfies module', async () => {
+    const license = 'GFDL-1.1-invariants-or-later';
+
+    let error;
+    try {
+      await check(license);
+    } catch (e) {
+      error = e;
+    } finally {
+      expect(error.message).toMatch('GFDL-1.x licenses are temporary unallowed. There\'s an issue pending to be solved. 🙏');
+    }
+  });
+});
