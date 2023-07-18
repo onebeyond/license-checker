69352956383df076e0f88cc76c227bb09bb4661d
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2023-07-18 18:19:25 +0200

    chore: configure yargs

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@ const yargs = require('yargs')(process.argv.slice(2));
 
 // https://github.com/yargs/yargs/blob/main/docs/advanced.md#commanddirdirectory-opts
 module.exports = yargs
+  .parserConfiguration({ 'camel-case-expansion': false })
   .commandDir('commands')
   .demandCommand()
   .help()
