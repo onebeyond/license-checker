d793f652acba67afd0c0218453de2bed5673a261
David Yusta <david.yusta@guidesmiths.com>
2023-09-15 08:58:40 +0200

    feat: use yargs check to validate scan input

--- cli.js
+++ cli.js
@@ -1,10 +1,12 @@
 const yargs = require('yargs')(process.argv.slice(2));
+const { checkArgs } = require('./src/utils');
 
 // https://github.com/yargs/yargs/blob/main/docs/advanced.md#commanddirdirectory-opts
 module.exports = yargs
   .parserConfiguration({ 'camel-case-expansion': false })
   .commandDir('commands')
   .demandCommand()
+  .check(checkArgs)
   .help()
   .alias('help', 'h')
   .argv;
